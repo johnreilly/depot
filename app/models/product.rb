b31ceb983534b316c22a46eeaed246cde695e636
John Reilly <jr@trms.com>
2009-04-11 15:51:07 -0500

    product title now must be at least 10 chars in length

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   validates_presence_of     :title, :description, :image_url
   validates_numericality_of :price
   validates_uniqueness_of   :title
+  validates_length_of       :title, :minimum => 10
   validates_format_of       :image_url,
                             :with => /\.(gif|jpg|png)$/i,
                             :message => 'must be a URL for GIF, JPG or PNG image.'
