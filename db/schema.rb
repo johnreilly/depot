90ae1ae9d56eedff81a7d9e049e8e1fd250abd54
John Reilly <jr@trms.com>
2009-04-11 15:28:42 -0500

    added test data migration for products

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090411195930) do
+ActiveRecord::Schema.define(:version => 20090411202713) do
 
   create_table "products", :force => true do |t|
     t.string   "title"
