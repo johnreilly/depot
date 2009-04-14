95a8230e09267e849450f393ff1a188bfbd79066
John Reilly <jr@trms.com>
2009-04-14 15:12:53 -0500

    added rjs response to update the sidebar cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,7 +8,11 @@ class StoreController < ApplicationController
     product = Product.find(params[:id])
     @cart = find_cart
     @cart.add_product(product)
-    redirect_to_index
+
+    respond_to do |format|
+      format.js
+    end
+    
   rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
     redirect_to_index("Invalid Product")
