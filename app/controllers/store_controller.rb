53b474c4a59375fbbb508d60a082ae614b367602
John Reilly <jr@trms.com>
2009-04-14 15:00:51 -0500

    adding an item now redirects back to store index. removed the add_to_cart view, because it is no longer needed

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -8,6 +8,7 @@ class StoreController < ApplicationController
     product = Product.find(params[:id])
     @cart = find_cart
     @cart.add_product(product)
+    redirect_to_index
   rescue ActiveRecord::RecordNotFound
     logger.error("Attempt to access invalid product #{params[:id]}")
     redirect_to_index("Invalid Product")
@@ -23,8 +24,8 @@ class StoreController < ApplicationController
     session[:cart] ||= Cart.new
   end
   
-  def redirect_to_index(msg)
-    flash[:notice] = msg
+  def redirect_to_index(msg = nil)
+    flash[:notice] = msg if msg
     redirect_to :action => "index"
   end
 end
