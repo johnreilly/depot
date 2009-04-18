e594b5baaf9aee1b4606b5e8e099f503acb4b4c7
John Reilly <jr@trms.com>
2009-04-18 16:17:09 -0500

    adding validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ActiveRecord::Base
+
+  validates_presence_of :name
+  validates_uniqueness_of :name
+  
+  attr_accessor :password_confirmation
+  validates_confirmation_of :password
+  
+  validate :password_non_blank
+  
+  
+  private
+  def password_non_blank
+    errors.add(:password, "Missing Password") if hashed_password.blank?
+  end
+
 end
