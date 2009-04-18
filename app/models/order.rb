49528be096cddb1e45185e5505971ac376c70458
John Reilly <jr@trms.com>
2009-04-18 15:58:37 -0500

    Order objects can add line_items to themselves based on the contents of a cart

--- app/models/order.rb
+++ app/models/order.rb
@@ -11,5 +11,11 @@ class Order < ActiveRecord::Base
   
   has_many :line_items
   
+  def add_line_items_from_cart(cart)
+    cart.items.each do |item|
+      li = LineItem.from_cart_item(item)
+      line_items << li
+    end
+  end
   
 end
