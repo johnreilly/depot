caea0a8a12cfc3fd6a16240652a3c5c8a88ab88f
John Reilly <jr@trms.com>
2009-11-18 13:51:21 -0600

    fixed copy/paste bug introduced in F2

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,11 +1,13 @@
 class AdminController < ApplicationController
   def login
     if request.post?
-      user = User.authenticate(params[:name], params[:password]) if user
-      session[:user_id] = user.id
-      redirect_to(:action => "index")
-    else
-      flash.now[:notice] = "Invalid user/password combination"
+      user = User.authenticate(params[:name], params[:password]) 
+      if user
+        session[:user_id] = user.id
+        redirect_to(:action => "index")
+      else
+        flash.now[:notice] = "Invalid user/password combination"
+      end
     end
   end
 
