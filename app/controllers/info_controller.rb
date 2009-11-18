64baef128367bea45731a3d8b98f32f4ba2c7e82
John Reilly <jr@trms.com>
2009-11-18 14:39:01 -0600

    added json report of orders

--- app/controllers/info_controller.rb
+++ app/controllers/info_controller.rb
@@ -8,6 +8,7 @@ class InfoController < ApplicationController
       format.html
       format.atom { render :layout => false }
       format.xml { render :layout => false } 
+      format.json { render :layout => false , :json => @product.to_json(:include => :orders) }
     end
   end
 
