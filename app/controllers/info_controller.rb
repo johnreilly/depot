6724e64059a771ca4bcd4752c773e6a2a50753a1
John Reilly <jr@trms.com>
2009-11-18 14:23:50 -0600

    adding basic info controller, responds with XML of orders for a product

---
+++ app/controllers/info_controller.rb
@@ -0,0 +1,12 @@
+class InfoController < ApplicationController
+  skip_before_filter :authorize
+  
+  def who_bought
+    @product = Product.find(params[:id]) 
+    @orders = @product.orders 
+    respond_to do |format|
+      format.xml { render :layout => false } 
+    end
+  end
+
+end
