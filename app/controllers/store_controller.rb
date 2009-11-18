f3cb9dd4fb19311bb4dac0950fa538f661dd1d37
John Reilly <jr@trms.com>
2009-11-18 14:11:37 -0600

    cleaning up some duplication

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,14 +1,13 @@
 class StoreController < ApplicationController
   skip_before_filter :authorize
+  before_filter :find_cart, :except => :empty_cart
   
   def index
     @products = Product.find_products_for_sale
-    @cart = find_cart
   end
 
   def add_to_cart
     product = Product.find(params[:id])
-    @cart = find_cart
     @current_item = @cart.add_product(product)
 
     respond_to do |format|
@@ -27,7 +26,6 @@ class StoreController < ApplicationController
   end
 
   def checkout
-    @cart = find_cart
     if @cart.items.empty?
       redirect_to_index("Your cart is empty!")
     else
@@ -36,7 +34,6 @@ class StoreController < ApplicationController
   end
   
   def save_order
-    @cart = find_cart
     @order = Order.new(params[:order])
     @order.add_line_items_from_cart(@cart)
     if @order.save
@@ -49,7 +46,7 @@ class StoreController < ApplicationController
   
   private
   def find_cart
-    session[:cart] ||= Cart.new
+    @cart = (session[:cart] ||= Cart.new)
   end
   
   def redirect_to_index(msg = nil)
